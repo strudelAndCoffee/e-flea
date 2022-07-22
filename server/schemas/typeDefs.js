40d698a4534a0a213f8430ac02c5530e744e5e49
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-22 00:08:28 -0500

    finished typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,32 @@ const typeDefs = gql`
         orders: [Order]
     }
 
-    
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        categories: [Category]
+        products(category: ID, name: String): [Product]
+        product(_id: ID!): Product
+        user: User
+        orders(user: ID): [Order]
+        order(_id: ID!): Order
+    }
+
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, userName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        addProduct(name: String!, description: String, image: String, price: Float!, quantity: Number, category: ID!): Product
+        addCategory(name: String!): Category
+        updateUser(firstName: String, lastName: String, userName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        deleteUser(_id: ID!): User
+        deleteProduct(_id: ID!): Product
+        deleteCategory(_id: ID!): Category
+        login(email: String!, password: String!): Auth
+    }
 `;
 
 module.exports = typeDefs;
