0281e4cf58ba3c3250e0a0bc33b24d2708234be8
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-22 17:35:30 -0500

    added add user mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,6 +60,14 @@ const resolvers = {
             }
             throw new AuthenticationError("Not logged in");
         }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return ( token, user };
+        },
     }
 };
 
