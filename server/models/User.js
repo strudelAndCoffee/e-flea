1ca06b6ad9937a789297e09b6a11b7cff400a475
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-21 23:21:20 -0500

    set up User model

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mogoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    orders: [Order.schema],
+    rating: [Plus.schema]
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
