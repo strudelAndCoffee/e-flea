d1e772dbe31cd6157b350345d9644cf052753469
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-07-27 20:38:29 -0500

    deleted plus model

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,5 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
-// const Plus = require('./Plus');
 
 module.exports = { User, Product, Category, Order };
