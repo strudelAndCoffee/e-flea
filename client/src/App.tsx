24d331fa5451d0008f6c28846cc35ddba9a1eb0f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-19 22:41:38 -0600

    start header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components'
+
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../css/header.css'
+
+export default function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="title">eFlea</h1>
+      <div>Search</div>
+      <div>Menu</div>
+      <div>Cart</div>
+    </header>
+  )
+}
