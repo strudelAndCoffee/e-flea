e84a7a5cda1d9cf50826ba7bc684f4cad8228bbb
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-22 17:31:36 -0600

    add panel and close btn to cart

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,3 +1,19 @@
+import { useState } from 'react'
+
 export default function Cart() {
-  return <div className="cart">Cart</div>
+  const [openCart, setOpenCart] = useState(false)
+
+  return (
+    <div className="cart-container">
+      <button onClick={() => setOpenCart((prev) => !prev)}>Cart</button>
+
+      <div className={openCart ? 'cart-panel-overlay' : 'hide'}>
+        <div className="cart-panel">
+          <button className="close-cart-btn" onClick={() => setOpenCart(false)}>
+            &times;
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
