ea77ddaac73b8a144b7746155567d0632996bd9d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-22 03:56:47 -0600

    start cart as popover

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom'
+import { Popover, Transition } from '@headlessui/react'
 import '../css/header.css'
 
 export default function Header() {
   return (
     <header className="header-container">
       <h1 className="title">eFlea</h1>
+
+      <Popover className="cart">
+        <Popover.Button>Cart</Popover.Button>
+        <Popover.Panel className="cart-panel"></Popover.Panel>
+      </Popover>
+
       <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/vendor/1">Vendor</Link>
       </nav>
-      <div className="menu">Menu</div>
     </header>
   )
 }
