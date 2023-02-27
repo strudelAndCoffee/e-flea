94312a08b3068f7eef5237516af2a056331d0602
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-26 21:10:07 -0600

    install material icons

--- client/src/components/Main/Product.tsx
+++ client/src/components/Main/Product.tsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from 'react'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 import { DoggoImg } from '../'
 
 export default function Product() {
   return (
     <Grid item xs={4}>
-      <Paper elevation={2} square>
+      <Paper elevation={3} square>
         <DoggoImg />
+        <Box paddingX={1}>
+          <Typography variant="h5" component="h3">
+            Doggo
+          </Typography>
+          <Typography variant="subtitle1" component="p">
+            A doggo, ready to please
+          </Typography>
+        </Box>
       </Paper>
     </Grid>
   )
