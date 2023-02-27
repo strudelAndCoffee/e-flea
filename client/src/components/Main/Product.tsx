b8dc75effbf6a3e93d2d4d0478ec5be73dc3fbf1
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-26 21:37:24 -0600

    finish product card layout

--- client/src/components/Main/Product.tsx
+++ client/src/components/Main/Product.tsx
@@ -2,6 +2,8 @@ import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Rating from '@mui/material/Rating'
+// import { } from '@mui/icons-material'
 
 import { DoggoImg } from '../'
 
@@ -14,9 +16,41 @@ export default function Product() {
           <Typography variant="h5" component="h3">
             Doggo
           </Typography>
-          <Typography variant="subtitle1" component="p">
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1,
+            }}
+          >
+            {/* rating value={null} for no rating */}
+            <Rating
+              name="read-only"
+              value={4.5}
+              precision={0.5}
+              size="small"
+              readOnly
+            />
+            <Typography variant="body2" component="p">
+              4.5
+            </Typography>
+            <Typography variant="body2" component="p">
+              (3 billion reviews)
+            </Typography>
+          </Box>
+          <Typography variant="body1" component="p" marginY={1}>
             A doggo, ready to please
           </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <Typography variant="h6" component="h4">
+              $1,000,000
+            </Typography>
+          </Box>
         </Box>
       </Paper>
     </Grid>
