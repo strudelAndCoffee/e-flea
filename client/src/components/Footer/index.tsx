eec8a7035db62652f1850d63d4aa6d9da7312c10
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-03-01 02:23:40 -0600

    finish footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,13 +1,27 @@
 import AppBar from '@mui/material/AppBar'
+import GitHub from '@mui/icons-material/GitHub'
 
 export default function Footer() {
   return (
     <AppBar
       position="fixed"
       color="inherit"
-      sx={{ top: 'auto', bottom: 0, textAlign: 'center' }}
+      sx={{
+        top: 'auto',
+        bottom: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingY: 0.5,
+      }}
     >
-      footer
+      <a
+        href="https://github.com/strudelAndCoffee"
+        target="_blank"
+        className="footer-link"
+      >
+        <GitHub fontSize="small" sx={{ marginRight: 0.5 }} /> strudelAndCoffee
+      </a>
     </AppBar>
   )
 }
