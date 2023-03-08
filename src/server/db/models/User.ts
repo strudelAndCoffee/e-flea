141442e1c0db976b6df54150e919d9dae8c1d45a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-03-08 00:30:18 -0600

    add to User model

--- src/server/db/models/User.ts
+++ src/server/db/models/User.ts
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
@@ -20,6 +28,10 @@ const UserSchema = new mongoose.Schema({
     default: [],
   },
   favorite_vendors: {
+    type: [String],
+    default: [],
+  },
+  purchases: {
     type: [String],
     default: [],
   },
