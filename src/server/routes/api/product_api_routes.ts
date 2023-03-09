d0afb6c3776d60da2e020b58b63510593ce917c5
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-03-09 02:54:27 -0600

    add withAuth to product and user routes

--- src/server/routes/api/product_api_routes.ts
+++ src/server/routes/api/product_api_routes.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import { ProductModel } from '../../db/models'
+import withAuth from '../../utils/withAuth'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
     const products = await ProductModel.find({})
+    if (!products) res.status(400).json({ message: 'No products found.' })
+
     res.json({ products })
   } catch (err) {
     res.json(err)
@@ -15,13 +18,16 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const product = await ProductModel.find({ _id: req.params.id })
+    if (!product)
+      res.status(400).json({ message: 'No product found with that ID.' })
+
     res.json({ product })
   } catch (err) {
     res.json(err)
   }
 })
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   const new_product = new ProductModel(req.body)
   try {
     const response = await new_product.save()
@@ -31,20 +37,26 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.put('/', async (req, res) => {
+router.put('/', withAuth, async (req, res) => {
   const query = { _id: req.body.product_id }
   const update = { ...req.body.update_fields }
   try {
     const updated_product = await ProductModel.findOneAndUpdate(query, update)
+    if (!updated_product)
+      res.status(400).json({ message: 'No product found with that ID.' })
+
     res.json({ updated_product })
   } catch (err) {
     res.json(err)
   }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const deleted_product = await ProductModel.findByIdAndDelete(req.params.id)
+    if (!deleted_product)
+      res.status(400).json({ message: 'No product found with that ID.' })
+
     res.json({ deleted_product_id: deleted_product?._id })
   } catch (err) {
     res.json(err)
