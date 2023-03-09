6bd9655baca484d0853715f05b24d650ffe11de8
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-03-08 19:57:09 -0600

    update model types and schemas

--- src/server/db/models/Product.ts
+++ src/server/db/models/Product.ts
@@ -1,38 +1,33 @@
 import mongoose, { Schema } from 'mongoose'
 
-export interface ProductType {
-  name: string
-  description: string
-  rating_total?: number
+type RatingSchemaType = {
+  rating_total: number
   rating_scores?: number[]
-  reviews?: number
-  price: number
-  categories: string[] | []
-  tags?: Schema.Types.ObjectId[]
+  reviews: number
+}
+type ImageSchemaType = {
   image_url: string
   image_upload?: any
   image_alt: string
+}
+export interface ProductType {
+  name: string
   vendor_id: Schema.Types.ObjectId
+  description: string
+  price: number
+  categories: string[] | []
+  tags?: Schema.Types.ObjectId[]
+  rating: RatingSchemaType
+  image: ImageSchemaType
 }
 
-const ProductSchema = new Schema<ProductType>({
-  name: {
-    type: String,
-    minlength: 3,
-    maxlength: 256,
-    required: [true, 'Your product needs a name.'],
-  },
-  description: {
-    type: String,
-    minlength: 3,
-    maxlength: 256,
-    required: [true, 'Please describe your product.'],
-  },
+const RatingSchema = new Schema<RatingSchemaType>({
   rating_total: {
     type: Number,
     min: 0,
     max: 5,
     default: 0,
+    required: true,
   },
   rating_scores: [
     {
@@ -45,25 +40,11 @@ const ProductSchema = new Schema<ProductType>({
     type: Number,
     min: 0,
     default: 0,
+    required: true,
   },
-  price: {
-    type: Number,
-    min: 0,
-    required: [true, 'You need to get paid!'],
-  },
-  categories: [
-    {
-      type: String,
-      lowercase: true,
-      required: [true, 'Your product needs a category to be found!'],
-    },
-  ],
-  tags: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'tags',
-    },
-  ],
+})
+
+const ImageSchema = new Schema<ImageSchemaType>({
   image_url: {
     type: String,
     default: 'https://random.dog/068fc183-d4e3-4780-b01c-6cce0d019d13.jpg',
@@ -86,10 +67,51 @@ const ProductSchema = new Schema<ProductType>({
       'Default alt text will be used if no alt text is provided.',
     ],
   },
+})
+
+const ProductSchema = new Schema<ProductType>({
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 256,
+    required: [true, 'Your product needs a name.'],
+  },
   vendor_id: {
     type: Schema.Types.ObjectId,
     ref: 'vendors',
     required: [true, 'Please provide the vendor ID.'],
+  },
+  description: {
+    type: String,
+    minlength: 3,
+    maxlength: 256,
+    required: [true, 'Please describe your product.'],
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: [true, 'You need to get paid!'],
+  },
+  categories: [
+    {
+      type: String,
+      lowercase: true,
+      required: [true, 'Your product needs a category to be found!'],
+    },
+  ],
+  tags: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'tags',
+    },
+  ],
+  rating: {
+    type: RatingSchema,
+    required: true,
+  },
+  image: {
+    type: ImageSchema,
+    required: true,
   },
 })
 
--- src/server/db/models/Vendor.ts
+++ src/server/db/models/Vendor.ts
@@ -1,32 +1,18 @@
 import mongoose, { Schema } from 'mongoose'
 
-export interface VendorType {
-  store_title: string
-  owner_id: Schema.Types.ObjectId
-  product_ids?: Schema.Types.ObjectId[]
+type ImageSchemaType = {
   store_image_url: string
   store_image_upload?: any
   store_image_alt: string
 }
+export interface VendorType {
+  store_title: string
+  owner_id: Schema.Types.ObjectId
+  product_ids?: Schema.Types.ObjectId[]
+  image: ImageSchemaType
+}
 
-const VendorShema = new Schema<VendorType>({
-  store_title: {
-    type: String,
-    minlength: 3,
-    maxlength: 128,
-    required: [true, 'Your store needs a name to be found!'],
-    unique: true,
-  },
-  owner_id: {
-    type: Schema.Types.ObjectId,
-    required: [true, 'Please provide the owner ID.'],
-  },
-  product_ids: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'products',
-    },
-  ],
+const ImageSchema = new Schema<ImageSchemaType>({
   store_image_url: {
     type: String,
     default: 'https://random.dog/068fc183-d4e3-4780-b01c-6cce0d019d13.jpg',
@@ -48,6 +34,30 @@ const VendorShema = new Schema<VendorType>({
       true,
       'Default alt text will be used if no alt text is provided.',
     ],
+  },
+})
+
+const VendorShema = new Schema<VendorType>({
+  store_title: {
+    type: String,
+    minlength: 3,
+    maxlength: 128,
+    required: [true, 'Your store needs a name to be found!'],
+    unique: true,
+  },
+  owner_id: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'Please provide the owner ID.'],
+  },
+  product_ids: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'products',
+    },
+  ],
+  image: {
+    type: ImageSchema,
+    required: true,
   },
 })
 
